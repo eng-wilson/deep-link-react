0ef6e3094038e58741ae0016cd0b38ab3ba327da
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-02-23 17:01:53 -0300

    chore: log added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   const getQueryVariable = (path) => {
     const query = path.split("?path=")[1];
 
+    console.log(query);
+
     window.location.replace("eurekka://deep.eurekka.me/" + query);
   };
 
