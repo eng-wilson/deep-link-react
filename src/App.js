89cd86b574677d5d749ba98226ef1a7b30e47777
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-05-06 09:54:32 -0300

    style: css added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import logo from "./assets/icon.png";
+import "./App.css";
 
 function App() {
   const getQueryVariable = (path) => {
@@ -16,7 +18,35 @@ function App() {
   useEffect(() => {
     getQueryVariable(document.URL);
   }, []);
-  return <div></div>;
+
+  return (
+    <main>
+      <div className="content">
+        <img src={logo} alt="Eurekka logo" />
+        <h1>Estamos te redirecionando ao nosso App</h1>
+
+        <p>
+          Caso o redirecionamento esteja demorando, você pode clicar no botão
+          abaixo
+        </p>
+
+        <button onClick={() => getQueryVariable(document.URL)}>
+          Abrir App
+        </button>
+      </div>
+
+      <footer>
+        <p>
+          Lembre-se de verificar se o nosso aplicativo está instalado nesse
+          dispositivo
+        </p>
+        <span>
+          Em caso de dúvida, entre em contato com nosso suporte{" "}
+          <a href="mailto:contato@eurekka.me">contato@eurekka.me</a>
+        </span>
+      </footer>
+    </main>
+  );
 }
 
 export default App;
